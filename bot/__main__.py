e776fa583507f8aef2d9587e7c9398166b8f658e
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2025-01-10 17:16:38 +0100

    Removed useless code

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,4 @@
-import random
 import logging
-from time import sleep
-import traceback
 import re
 
 from pyrogram import filters
@@ -69,33 +66,6 @@ def work(_: Client, message: Message):
         logging.error(
             f"Error while sending message from {
                 message.chat.id} to {chat_id}: {e}"
-        )
-
-
-@app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward"]), group=1)
-def forward(client: Client, message: Message):
-    if len(message.command) > 1 and message.command[1].isdigit():
-        chat_id = int(message.command[1])
-        if chat_id:
-            try:
-                offset_id = 0
-                limit = 0
-                if len(message.command) > 2:
-                    limit = int(message.command[2])
-                if len(message.command) > 3:
-                    offset_id = int(message.command[3])
-                for msg in client.get_chat_history(
-                    chat_id, limit=limit, offset_id=offset_id
-                ):
-                    msg.copy(message.chat.id)
-                    sleep(random.randint(1, 5))
-            except Exception as e:
-                message.reply_text(f"Error:\n```{traceback.format_exc()}```")
-        else:
-            message.reply_text("Invalid Chat Identifier. Give me a chat id.")
-    else:
-        message.reply_text(
-            "Invalid Command\nUse /fwd {chat_id} {limit} {first_message_id}"
         )
 
 
